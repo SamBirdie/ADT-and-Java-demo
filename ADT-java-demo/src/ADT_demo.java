c798d132db84fd4698f146d5b9ef3bc775966113
SamBirdie <65365440+SamBirdie@users.noreply.github.com>
2021-01-10 17:59:46 +0200

    Hahmottelun alkua

---
+++ ADT-java-demo/src/ADT_demo.java
@@ -0,0 +1,15 @@
+
+public class ADT_demo {
+    public static void main(String[] args) {
+        // argumentteina toiminto; jos ei anneta tulostetaan valikko
+        System.out.println("testing");
+    }
+
+    private static toiminto1() {
+
+    }
+
+    private static  toiminto2() {
+
+    }
+}
