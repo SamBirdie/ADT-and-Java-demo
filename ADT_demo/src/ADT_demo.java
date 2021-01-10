89670d27412a8d1933417b0fe6765ecee7283d8e
SamBirdie <65365440+SamBirdie@users.noreply.github.com>
2021-01-10 19:42:44 +0200

    etenee

---
+++ ADT_demo/src/ADT_demo.java
@@ -0,0 +1,20 @@
+
+
+public class ADT_demo {
+    public static void main(String[] args) {
+        // argumentteina toiminto; jos ei anneta tulostetaan valikko
+        System.out.println("testing");
+        exampleWithHashMap();
+        exampleWithTree();
+    }
+
+    private static void exampleWithHashMap() {
+
+        System.out.println("HashMap-example here");
+    }
+
+    private static  void exampleWithTree() {
+
+        System.out.println("Tree-example here");
+    }
+}
