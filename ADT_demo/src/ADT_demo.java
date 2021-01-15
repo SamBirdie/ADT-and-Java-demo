df33ce331b52c66b0c5fbc292a9d21ac81616118
SamBirdie <65365440+SamBirdie@users.noreply.github.com>
2021-01-15 23:02:29 +0200

    More is more

--- ADT_demo/src/ADT_demo.java
+++ ADT_demo/src/ADT_demo.java
@@ -1,11 +1,23 @@
 
 
 public class ADT_demo {
+
     public static void main(String[] args) {
         // argumentteina toiminto; jos ei anneta tulostetaan valikko
 
         boolean getHashmapDemoDatabase = false;
         boolean getTreeDemoDatabase = false;
+
+        switch (args[0]) {
+            case "hashmap":
+                // TODO
+                getHashmapDemoDatabase = true;
+                break;
+
+            case "tree":
+                getTreeDemoDatabase = true;
+                break;
+        }
 
         if (getHashmapDemoDatabase)
             exampleWithHashMap();
@@ -15,10 +27,12 @@ public class ADT_demo {
     }
 
     private static void exampleWithHashMap() {
+        // TODO
         System.out.println("HashMap-example here");
     }
 
     private static void exampleWithTree() {
+        // TODO
         System.out.println("Tree-example here");
     }
 }
